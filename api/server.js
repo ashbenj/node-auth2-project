bc0120f6144fdaf834198f65ea7318fc33463f50
Asha <ashalbenjamin@gmail.com>
2020-04-28 18:15:38 -0500

    auth-router setup

---
+++ api/server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cosrs');
+
+const authRouter = require('');
---
+++ auth/auth-router.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+
+const Users = require('../users/users-model');
+
+//for endpoints beginning with /api/auth
+router.post('/register', (req, res) => {
+	let user = req.body;
+	const hash = bcrypt.hashSync(user.password, 10);
+	user.password = hash;
+
+	Users.add(user)
+		.then((saved) => {
+			res.status(500).json(error);
+		})
+		.catch((error) => {
+			res.status(500).json(error);
+		});
+});
+
+router.post('/login', (req, res) => {
+	let { username, password } = req.body;
+
+	Users.findBy({ username })
+		.first()
+		.then((user) => {
+			if (user && bycrypt.compareSync(password, user.password)) {
+				res.status(200).json({
+					message: `Welcome ${user.username}!`,
+				});
+			} else {
+				res.status(401).json({ message: 'Invalid Credentials' });
+            }
+        })
+            .catch(error => {
+                res.status(500).json(error)
+            })
+        });
+        
+        module.exports = router;
+});
